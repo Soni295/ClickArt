74b0a9e490fc704c65142aec58657ee8321b07f0
Soni295 <Sionandrieux@hotmail.com>
2020-06-27 00:02:44 -0300

    Updates

--- frontreact/src/Componentes/Reutilisable/VentanaPerfil.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default (props) => {
-  return (
-    <div style={props.visible} className="ventana">
-      {props.texto}
-    </div>
-  );
-};
---
+++ precache-manifest.0c688e96c2c818031cfe538e6aa395b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4adf0e74273594661de014b4c6363a01",
+    "url": "/ClickArt/index.html"
+  },
+  {
+    "revision": "823011d723debae3c82b",
+    "url": "/ClickArt/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "4379bafe2331e5ea3d49",
+    "url": "/ClickArt/static/css/main.3e1a2e6b.chunk.css"
+  },
+  {
+    "revision": "823011d723debae3c82b",
+    "url": "/ClickArt/static/js/2.12fa83d3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ClickArt/static/js/2.12fa83d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4379bafe2331e5ea3d49",
+    "url": "/ClickArt/static/js/main.677ff084.chunk.js"
+  },
+  {
+    "revision": "3761d6bfd9854c0d9735",
+    "url": "/ClickArt/static/js/runtime-main.57d50c7d.js"
+  }
+]);
